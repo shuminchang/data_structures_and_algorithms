ebd4a6f77f22c8ad2f8c44f511dc05f86ab0aa39
shuminchang <magic2313@gmail.com>
2023-01-18 15:13:34 +0800

    insert, find value

--- data_structures/hash_table/hashtable.py
+++ data_structures/hash_table/hashtable.py
@@ -1,7 +1,46 @@
+BLANK = object()
+
 class HashTable:
 	def __init__(self, capacity):
 		# self.capacity = capacity
-		self.values = capacity * [None]
+		# self.values = capacity * [None]
+		self.values = capacity * [BLANK]
 
 	def __len__(self):
 		return len(self.values)
+
+	def __setitem__(self, key, value):
+		# self.values.append(value) # ignores the key, and the length will increase
+		index = hash(key) % len(self)
+		"""
+			hash(): turn an arbitary key into a numeric hash value
+			%: to constrain the resulting index within the available address space
+
+			hash code collisions, PYTHONHASHSEED
+		"""
+		self.values[index] = value
+
+	def __getitem__(self, key):
+		index = hash(key) % len(self)
+		value = self.values[index]
+		if value is BLANK:
+			"""
+				is: compare identites
+				==: compare values
+			"""
+			raise KeyError(key)
+		return value
+
+	def __contains__(self, key):
+		try:
+			self[key]
+		except KeyError as e:
+			return False
+		else:
+			return True
+
+	def get(self, key, default=None):
+		try:
+			return self[key]
+		except:
+			return default
--- data_structures/hash_table/test_hashtable.py
+++ data_structures/hash_table/test_hashtable.py
@@ -1,5 +1,9 @@
-from hashtable import HashTable
+from hashtable import HashTable, BLANK
+import pytest
 
+"""
+Define a Custom HashTable Class
+"""
 def test_should_create_hashtable():
 	assert HashTable(capacity=100) is not None
 
@@ -8,7 +12,8 @@ def test_shourld_report_capacity():
 
 def test_should_create_empty_value_slots():
 	# Given
-	expected_values = [None, None, None]
+	# expected_values = [None, None, None]
+	expected_values = [BLANK, BLANK, BLANK]
 	hash_table = HashTable(capacity=3)
 
 	# When
@@ -16,3 +21,72 @@ def test_should_create_empty_value_slots():
 
 	# Then
 	assert actual_values == expected_values
+
+"""
+Insert a Key-Value Pair
+"""
+def test_should_insert_key_value_pairs():
+	hash_table = HashTable(capacity=100)
+
+	hash_table["hola"] = "hello"
+	hash_table[98.6] = 37
+	hash_table[False] = True
+
+	assert "hello" in hash_table.values
+	assert 37 in hash_table.values
+	assert True in hash_table.values
+
+	assert len(hash_table) == 100
+
+@pytest.mark.skip
+def test_should_not_shrink_when_removing_elements():
+	pass
+
+def test_should_not_contain_none_value_when_created():
+	assert None not in HashTable(capacity=100).values
+
+def test_should_insert_none_value():
+	# happy path
+	hash_table = HashTable(capacity=100)
+	hash_table["key"] = None
+	assert None in hash_table.values
+
+"""
+Find a Value by Key
+"""
+@pytest.fixture
+def hash_table():
+	hash_table = HashTable(capacity=100)
+	hash_table["hola"] = "hello"
+	hash_table[98.6] = 37
+	hash_table[False] = True
+	return hash_table
+
+def test_should_find_value_by_key(hash_table):
+	assert "hello" in hash_table.values
+	assert 37 in hash_table.values
+	assert True in hash_table.values
+
+def test_should_raise_error_on_missing_key():
+	hash_table = HashTable(capacity=100)
+	with pytest.raises(KeyError) as exception_info:
+		hash_table["missing_key"]
+	assert exception_info.value.args[0] == "missing_key"
+
+def test_should_find_key(hash_table):
+	assert "hola" in hash_table
+
+def test_should_not_find_key(hash_table):
+	assert "missing_key" not in hash_table
+
+def test_should_get_value(hash_table):
+	assert hash_table.get("hola") == "hello"
+
+def test_should_get_none_when_missing_key(hash_table):
+	assert hash_table.get("missing_key") is None
+
+def test_should_get_default_value_when_missing_key(hash_table):
+	assert hash_table.get("missing_key", "default") == "default"
+
+def test_should_get_value_with_default(hash_table):
+	assert hash_table.get("hola", "default") == "hello"
