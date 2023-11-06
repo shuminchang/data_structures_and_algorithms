441fd00bddfea3cb00cd3d98bd536592c06a0853
shuminchang <magic2313@gmail.com>
2023-11-06 17:04:52 +0800

    bst add methods

--- data_structures/tree/binary_search_tree.py
+++ data_structures/tree/binary_search_tree.py
@@ -25,6 +25,18 @@ class BinarySearchTree:
 						current_node.right = new_node
 						return
 					current_node = current_node.right
+					
+	def insert_recursive(self, value):
+		self.root = self._insert_recursive(self.root, value)
+		
+	def _insert_recursive(self, node, value):
+		if node is None:
+			return Node(value)
+		if value < node.value:
+			node.left = self._insert_recursive(node.left, value)
+		elif value > node.value:
+			node.right = self._insert_recursive(node.right, value)
+		return node
 	
 	def lookup(self, value):
 		if self.root == None:
@@ -52,6 +64,30 @@ class BinarySearchTree:
 			if current_node.right:
 				queue.append(current_node.right)
 		return arr
+	
+	def deleteNode(self, value):
+		self.root = self._deleteNode(self.root, value)
+		
+	def _deleteNode(self, root, value):
+		if root is None:
+			return root
+		if value < root.value:
+			root.left = self._deleteNode(root.left, value)
+		elif value > root.value:
+			root.right = self._deleteNode(root.right, value)
+		else:  # why else?
+			if root.left is None:
+				temp = root.right
+				root = None
+				return temp
+			elif root.right is None:
+				temp = root.left
+				root = None
+				return temp
+			temp = self.minValueNode(root.right)
+			root.value = temp.value
+			root.right = self._deleteNode(root.right, temp.value)
+		return root
 		
 	def print_tree(self):
 		if self.root != None:
@@ -63,6 +99,12 @@ class BinarySearchTree:
 			print(str(curr_node.value))
 			self.printt(curr_node.right)
 			
+	def minValueNode(self, node):
+		current = node
+		while current.left is not None:
+			current = current.left
+		return current
+			
 bst = BinarySearchTree()
 bst.insert(9)
 bst.insert(4)
@@ -73,3 +115,15 @@ bst.insert(15)
 bst.insert(1)
 print(bst.print_tree())
 print(bst.breadthFirstSearch())
+
+tree = BinarySearchTree()
+tree.insert_recursive(8)
+tree.insert_recursive(3)
+tree.insert_recursive(1)
+tree.insert_recursive(6)
+tree.insert_recursive(7)
+tree.insert_recursive(10)
+tree.insert_recursive(14)
+tree.insert_recursive(4)
+tree.deleteNode(3)
+print(tree.breadthFirstSearch())
